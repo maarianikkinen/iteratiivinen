06d3f1ef8dbf40fb0a18e4f2e05066533985b6c3
maarianikkinen <148542352+maarianikkinen@users.noreply.github.com>
2024-03-14 13:59:25 +0200

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="moi.WebForm1" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div class="ylin">
+                <asp:DropDownList ID="Kaupunki" runat="server" AutoPostBack="true" OnSelectedIndexChanged="Kaupunki_SelectedIndexChanged">
+                    <asp:ListItem Value="Valitsekaupunki">Valitse kaupunki</asp:ListItem>    
+                    <asp:ListItem Value="Lahti">Lahti</asp:ListItem>
+                    <asp:ListItem Value="Tampere">Tampere</asp:ListItem>
+                    <asp:ListItem Value="Turku">Turku</asp:ListItem>
+                </asp:DropDownList>
+            </div>
+            <div class ="toka">
+                <asp:DropDownList ID="Mittaus" runat="server">
+                    <asp:ListItem Value="Valitsemittauspiste">Mittauspiste</asp:ListItem>
+                </asp:DropDownList>
+            </div>
+            <div class ="kolmas">
+                <button class="etsi">Etsi tiedot</button>
+            </div>
+        </div>
+    </form>
+
+    <div id="mittari">
+    </div>
+</body>
+</html>
+
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace moi
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // You can initialize data here if needed
+            }
+        }
+
+        protected void Kaupunki_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Clear existing items
+            Mittaus.Items.Clear();
+
+            // Populate second dropdown based on selected city
+            switch (Kaupunki.SelectedValue)
+            {
+                case "Lahti":
+                    Mittaus.Items.Add(new ListItem("Mittauspiste", "Mittauspiste"));
+                    Mittaus.Items.Add(new ListItem("Lotila", "Lotila"));
+                    Mittaus.Items.Add(new ListItem("Kärpäsenmäki", "Kärpäsenmäki"));
+                    break;
+                case "Tampere":
+                Mittaus.Items.Add(new ListItem("Mittauspiste", "Mittauspiste"));
+                    Mittaus.Items.Add(new ListItem("Onkiniemi", "Onkiniemi"));
+                    Mittaus.Items.Add(new ListItem("Petsamo", "Petsamo"));
+                    
+                    break;
+                case "Turku":
+                Mittaus.Items.Add(new ListItem("Mittauspiste", "Mittauspiste"));
+                    Mittaus.Items.Add(new ListItem("Kupittaa", "Kupittaa"));
+                    Mittaus.Items.Add(new ListItem("Artukainen", "Artukainen"));
+                    Mittaus.Items.Add(new ListItem("Ihala", "Ihala"));
+                    break;
+                default:
+                    // Do nothing
+                    break;
+            }
+        }
+    }
+}
